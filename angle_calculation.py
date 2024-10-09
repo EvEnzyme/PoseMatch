d80d3ffa050952a8a8e59f7298882e7b75594669
EvEnzyme <Mj20011020@outlook.com>
2024-10-09 15:47:25 -0400

    Completed all-8 angle comparisons

--- angle_calculation.py
+++ angle_calculation.py
@@ -8,21 +8,47 @@ class PoseAngle:
         self.image = image
         self.frame_shape = frame_shape
         
+        # Left lankmark coordinates 
         self.left_shoulder = [self.landmarks[11].x, self.landmarks[11].y]
         self.left_elbow = [self.landmarks[13].x, self.landmarks[13].y]
         self.left_wrist = [self.landmarks[15].x, self.landmarks[15].y]
+        self.left_hip = [self.landmarks[23].x, self.landmarks[23].y]
+        self.left_knee = [self.landmarks[25].x, self.landmarks[25].y]
+        self.left_ankle = [self.landmarks[27].x, self.landmarks[27].y]
 
+        # Right landmark coordinates
         self.right_shoulder = [self.landmarks[12].x, self.landmarks[12].y]
         self.right_elbow = [self.landmarks[14].x, self.landmarks[14].y]
         self.right_wrist = [self.landmarks[16].x, self.landmarks[16].y]
+        self.right_hip = [self.landmarks[24].x, self.landmarks[24].y]
+        self.right_knee = [self.landmarks[26].x, self.landmarks[26].y]
+        self.right_ankle = [self.landmarks[28].x, self.landmarks[28].y]
+      
+        # Left joint angles
+        self.left_elbow_angle = self.calculate_angle(self.left_shoulder, self.left_elbow, self.left_wrist)
+        self.left_shoulder_angle = self.calculate_angle(self.left_hip, self.left_shoulder, self.left_elbow)
+        self.left_hip_angle = self.calculate_angle(self.left_knee, self.left_hip, self.left_shoulder)
+        self.left_knee_angle = self.calculate_angle(self.left_ankle, self.left_knee, self.left_hip)
 
-        self.left_elbow_angle_val = self.left_elbow_angle_func()
-        self.right_elbow_angle_val = self.right_elbow_angle_func()
+        # Right joint angles
+        self.right_elbow_angle = self.calculate_angle(self.right_shoulder, self.right_elbow, self.right_wrist)
+        self.right_shoulder_angle = self.calculate_angle(self.right_hip, self.right_shoulder, self.right_elbow)
+        self.right_hip_angle = self.calculate_angle(self.right_knee, self.right_hip, self.right_shoulder)
+        self.right_knee_angle = self.calculate_angle(self.right_ankle, self.right_knee, self.right_hip)
+
+        # Left joint angles display
+        self.display_angle(self.image, self.left_elbow_angle, self.left_elbow, self.frame_shape)
+        self.display_angle(self.image, self.left_shoulder_angle, self.left_shoulder, self.frame_shape)
+        self.display_angle(self.image, self.left_hip_angle, self.left_hip, self.frame_shape)
+        self.display_angle(self.image, self.left_knee_angle, self.left_knee, self.frame_shape)
         
-        self.display_angle(self.image, self.left_elbow_angle_val, self.left_elbow, self.frame_shape)
-        self.display_angle(self.image, self.right_elbow_angle_val, self.right_elbow, self.frame_shape)
-
-    def calculate_angle(self, vector_a, vector_b, vector_c):
+        # Right joint angles display
+        self.display_angle(self.image, self.right_elbow_angle, self.right_elbow, self.frame_shape)
+        self.display_angle(self.image, self.right_shoulder_angle, self.right_shoulder, self.frame_shape)
+        self.display_angle(self.image, self.right_hip_angle, self.right_hip, self.frame_shape)
+        self.display_angle(self.image, self.right_knee_angle, self.right_knee, self.frame_shape)
+       
+    def calculate_angle(self, vector_a, vector_b, vector_c) -> float:
         a = np.array(vector_a)
         b = np.array(vector_b)
         c = np.array(vector_c)
@@ -35,18 +61,15 @@ class PoseAngle:
             
         return angle
     
-    def left_elbow_angle_func(self):
-        return self.calculate_angle(self.left_shoulder, self.left_elbow, self.left_wrist)
-
-    def right_elbow_angle_func(self):
-        return self.calculate_angle(self.right_shoulder, self.right_elbow, self.right_wrist)
-
     def display_angle(self, image, angle, joint, frame_shape):
         text = str(int(angle))
         height, width, _ = frame_shape
         position =  tuple(np.multiply(joint, [width, height]).astype(int))
+
+        # 
+        position = (position[0] + 20, position[1] - 20)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 1
+        font_scale = 2
         colour = (255, 255, 255)
         thickness = 2
         
--- player_cam.py
+++ player_cam.py
@@ -1,13 +1,14 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import csv
 from angle_calculation import PoseAngle
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def judge(angle, image, joint1, joint2, frame_shape, standard=90):
-    height, width, _ = frame_shape
+def judge(angle, player_image, joint1, joint2, player_frame_shape, standard=90):
+    height, width, _ = player_frame_shape
     
     # Convert normalized landmarks to pixel coordinates
     joint1_pixel = tuple(np.multiply(joint1, [width, height]).astype(int))
@@ -21,63 +22,113 @@ def judge(angle, image, joint1, joint2, frame_shape, standard=90):
     line_colour = (0, G, R)  # has to be a tuple
 
     # Draw the line between elbow and wrist
-    cv2.line(image, joint1_pixel, joint2_pixel, line_colour, 20)
+    cv2.line(player_image, joint1_pixel, joint2_pixel, line_colour, 20)
 
-cap = cv2.VideoCapture(0)
+# Open the csv file for reading inside the while loop later
+csv_file_path = 'demo_angles.csv'
+csvfile = open(csv_file_path, newline='')
+csv_reader = list(csv.reader(csvfile)) # convert to list for direct indexing lateron
+# next(csv_reader) # skip the header
+
+line_index = 1 # start index for reading the csv, =1 to skip the header
+
+# Capture the demo video
+demo_cap = cv2.VideoCapture('assets/demo_vid.mov')
+
+# Capture the player camera feed
+player_cap = cv2.VideoCapture(0)
+
+# declare a list to hold demo angles read from the csv file
+demo_angles = []
+
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.8, min_tracking_confidence=0.8) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
+    while player_cap.isOpened():
+        ret_player, player_frame = player_cap.read()
 
-        if not ret:
+        if not ret_player:
             break
 
-        # Get frame dimensions dynamically
-        frame_shape = frame.shape
+        # Handle demo video
+        ret_demo, demo_frame = demo_cap.read()
+        if not ret_demo:
+            # If the demo video ends, reset the video capture (loop)
+            demo_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            ret_demo, demo_frame = demo_cap.read()
+
+        # Resize demo video to fit in the top-left corner
+        demo_frame_resized = cv2.resize(demo_frame, (810, 540))
         
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+        player_frame = cv2.flip(player_frame, 1)
+        # Get player_frame dimensions dynamically
+        player_frame_shape = player_frame.shape
+        
+        # Recolor player_image to RGB
+        player_image = cv2.cvtColor(player_frame, cv2.COLOR_BGR2RGB)
+        player_image.flags.writeable = False
       
         # Make detection
-        results = pose.process(image)
+        results = pose.process(player_image)
     
         # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        player_image.flags.writeable = True
+        player_image = cv2.cvtColor(player_image, cv2.COLOR_RGB2BGR)
+
+        # Overlay the demo video on the top-left corner of the player camera feed
+        player_image[0:demo_frame_resized.shape[0], 0:demo_frame_resized.shape[1]] = demo_frame_resized
+
         
         # Extract landmarks
-        if results.pose_landmarks:
-            
+        try:
+        
             landmarks = results.pose_landmarks.landmark
             
             # Initialize the PoseAngle class (ensure class name matches)
-            player = PoseAngle(image, frame_shape, landmarks)
-        
-        
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(0,0,240), thickness=5, circle_radius=10), 
-                                mp_drawing.DrawingSpec(color=(255,255,255), thickness=5, circle_radius=5) 
-                                 )
-        
-        try:
-            judge(player.left_elbow_angle_val, image,
-                  player.left_elbow, player.left_wrist,
-                  frame_shape, standard=90)
-            judge(player.right_elbow_angle_val, image,
-                  player.right_elbow, player.right_wrist,
-                  frame_shape, standard=90)
-        
+            player = PoseAngle(player_image, player_frame_shape, landmarks)
+            # Render detections
+            mp_drawing.draw_landmarks(player_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(0,0,240), thickness=5, circle_radius=10), 
+                                    mp_drawing.DrawingSpec(color=(255,255,255), thickness=5, circle_radius=5) 
+                                    )
+
+            # read a line from the csv file
+            csv_row = csv_reader[line_index]
+
+            demo_left_elbow_angle = float(csv_row[0])  # Left elbow angle
+            demo_right_elbow_angle = float(csv_row[1])
+            demo_left_shoulder_angle = float(csv_row[2])
+            demo_right_shoulder_angle = float(csv_row[3])
+
+            # if gets judged every few beats, the standard that's passed into the judge function will need to change
+            judge(player.left_elbow_angle, player_image,
+                    player.left_elbow, player.left_wrist,
+                    player_frame_shape, standard=demo_left_elbow_angle)
+            judge(player.right_elbow_angle, player_image,
+                    player.right_elbow, player.right_wrist,
+                    player_frame_shape, standard=demo_right_elbow_angle)
+            judge(player.left_shoulder_angle, player_image,
+                    player.left_shoulder, player.left_elbow,
+                    player_frame_shape, standard=demo_left_shoulder_angle)
+            judge(player.right_shoulder_angle, player_image,
+                    player.right_shoulder, player.right_elbow,
+                    player_frame_shape, standard=demo_right_shoulder_angle)
+
+
         except Exception as e:
             print(e)
 
+        # ensure that the line index for the csv file is not out of range
+        if line_index == len(csv_reader) - 1:
+                line_index = 1  # loop if reach end of file
+        else:
+            line_index += 1
 
-
-        cv2.imshow('Mediapipe Feed', image)
+        cv2.imshow('Player Feed with Demo', player_image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-    cap.release()
+    csvfile.close()
+    demo_cap.release()
+    player_cap.release()
     cv2.destroyAllWindows()
